7b62c25045367baa7d1d982395f2416af1028e49
nuvem <email.escolar174@gmail.com>
2024-11-09 00:33:32 -0300

    hah

--- python/poo/encapsulamento.py
+++ python/poo/encapsulamento.py
@@ -4,14 +4,19 @@ class Employee:
         self.name = name
         self.position = position
         self._salary = salary
+    
+    # Descritiva
     def __str__(self) -> str:
-        return f'Name: {self.name}\nPositition: {self.position}\nSalary: {self._salary}'
+        return f'Name: {self.name}\nPositition: {self.position}\nSalary: {self._salary}\n'
+    
+    # Métodos
     def increase(self, value):
         if value > 0:
             self._salary += value
             return f"Success. +{value}."
         else:
             return f"Failure. {value} <= 0."
+        
     def decrease(self, value):
         if value > 0 and value <= self._salary:
             self._salary -= value
@@ -19,7 +24,16 @@ class Employee:
         else: 
             return f"Failure.  {value} <= 0 or {value} > salary."
 
-Joao = Employee('Joao', 'Arquiteto', 5200)
+# Instanciação
+class Manager(Employee):
+    def __init__(self, name, position, salary = 0, bonus = 1.25) -> None:
+        super().__init__(name, position, salary)
+        self.bonus = bonus
+        self._salary = salary * bonus
+    def __str__(self) -> str:
+        return f'Name: {self.name}\nPositition: {self.position}\nSalary: {self._salary}\nBonus: {self.bonus}'
+
+Joao = Employee('Joao', 'Arquiteto', 10000)
 print(Joao, '\n')
 
 Joao.increase(2000)
@@ -27,3 +41,6 @@ print(Joao, '\n')
 
 Joao.decrease(700)
 print(Joao, '\n')
+
+Bob = Manager('Bob', 'Arquiteto', 10000)
+print(Bob, '\n')
---
+++ webcoding/13-javascript/main.js
@@ -0,0 +1,12 @@
+let discount = .25
+
+products = [
+    mouse = { 
+        name: 'Wireless Mouse', 
+        price: 700}, 
+    teclado = {
+        name: 'Wireless Keyboard',
+        price: 1200}
+    ];
+
+

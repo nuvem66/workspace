4c2d21a8db7e4d43c47578baec2cf22609ae29bf
nuvem66 <email.escolar174@gmail.com>
2024-11-14 16:40:40 -0300

    js

---
+++ webcoding/ 15-dialog-boxes/js/main.js
@@ -0,0 +1,15 @@
+// Shows simple mesage to the user;
+// alert("This is my message to the user.")
+
+// Shows a simple boolean function to the user;
+// let choice = confirm('Are you sure about it?')
+// if(choice){
+//     alert(choice)
+// } else{
+//     alert(choice)
+// }
+
+// Gets a simple input from the user;
+let string = prompt('Confirm it by typing "yes".')
+alert(string + 1)
+
